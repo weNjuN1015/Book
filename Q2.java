f640132b6e32c598adc4bb406202101753f3df02
weNjuN1015 <96854908+weNjuN1015@users.noreply.github.com>
2023-06-20 21:00:47 +0800

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,30 @@
+public class Q2 extends Q1 {
+    private String fileFormat;
+    
+    public EBook(String title, String author, double price, int quantity, String fileFormat) {
+        super(title, author, price, quantity);
+        this.fileFormat = fileFormat;
+    }
+    
+    
+    public String getFileFormat() {
+        return fileFormat;
+    }
+    
+    public void setFileFormat(String fileFormat) {
+        this.fileFormat = fileFormat;
+    }
+    
+    
+    public void openBook() {
+        System.out.println("The book is opened. You may start reading.");
+    }
+    
+    public void downloadBook() {
+        System.out.println("The book has begun downloading.");
+    }
+    
+    public boolean validateFormat() {
+        return fileFormat.equals(".pdf") || fileFormat.equals(".epub");
+    }
+}
