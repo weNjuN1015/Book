cc546b089e78c91d1b978d259653f9b95024655b
weNjuN1015 <96854908+weNjuN1015@users.noreply.github.com>
2023-06-21 10:47:07 +0800

    q3

---
+++ PhysicalBook.java
@@ -0,0 +1,35 @@
+//PhysicalBook.java is question3.
+
+
+public class PhysicalBook extends Book {
+    private double weight;
+    
+    public PhysicalBook(String title, String author, double price, int quantity, double weight) {
+        super(title, author, price, quantity);
+        this.weight = weight;
+    }
+    
+    
+    public double getWeight() {
+        return weight;
+    }
+    
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    
+    
+    public double calculateShippingCost() {
+        double shippingCost = 4.50;
+        double additionalWeight = weight - 500;
+        if (additionalWeight > 0) {
+            int extraCharges = (int) (additionalWeight / 250);
+            shippingCost += extraCharges;
+        }
+        return shippingCost;
+    }
+    
+    public double getWeightInPounds() {
+        return weight / 453.59237; 
+    }
+}
